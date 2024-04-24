d684366ec7e34babdd7f6436def8d8f5760adb6b
amisha0528 <aamisha_be21@thapar.edu>
2024-04-24 16:29:16 +0530

    clouds added

--- animation.js
+++ animation.js
@@ -15,6 +15,11 @@ class Animation {
       this.legPosition = 0;
       this.rotationAngle = 0;
       this.stopLen;
+      this.clouds = [
+                    { x: 100, y: 100, width: 100, height: 50, speed: 2 }, 
+                    { x: 300, y: 150, width: 120, height: 60, speed: 1.6 }, 
+                    { x: 500, y: 200, width: 110, height: 55, speed: 2.4 } 
+                ];
   }
 
   draw() {
@@ -23,15 +28,15 @@ class Animation {
       this.drawGoalPost();
       this.drawBall();
       this.drawPerson();
-
+      this.drawClouds();
       requestAnimationFrame(() => this.draw());
   }
 
   drawGoalPost() {
-      this.ctx.fillStyle = "#000000";
+      this.ctx.fillStyle = "#006367";
       this.ctx.fillRect(700, 370, 8, 200);
 
-      this.ctx.strokeStyle = "#000000";
+      this.ctx.strokeStyle = "#006367";
       this.ctx.lineWidth = 1;
       this.ctx.beginPath();
       for (let i = 0; i <= 18; i++) {
@@ -50,7 +55,7 @@ class Animation {
       this.ctx.save();
       this.ctx.translate(this.ballX, this.ballY);
 
-      this.ctx.fillStyle = "#cb7d87";
+      this.ctx.fillStyle = "#ff8c00";
       this.ctx.beginPath();
       this.ctx.rotate(this.rotationAngle * Math.PI / 180);
       this.ctx.arc(0, 0, 10, Math.PI, 0, false);
@@ -67,7 +72,7 @@ class Animation {
 
   drawPerson() {
     
-      this.ctx.fillStyle = "#000000";
+      this.ctx.fillStyle = "#566D7E";
 
       if (!this.kicked && this.ballX < 700) {
           this.personX += 0.5;
@@ -79,7 +84,7 @@ class Animation {
       this.ctx.lineWidth = 3.0;
       this.ctx.fillRect(this.personX + 10, this.personY - 10, 10, 40);
 
-      this.ctx.strokeStyle = "#000000";
+      this.ctx.strokeStyle = "#566D7E";
       this.ctx.beginPath();
       this.ctx.moveTo(this.personX + 10, this.personY + 5);
       this.ctx.lineTo(this.personX + 0, this.personY + 20);
@@ -140,4 +145,47 @@ class Animation {
           this.stopLen = 10;
       }
   }
+
+
+  drawCloud(cloud) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(cloud.x, cloud.y + cloud.height / 2);
+
+    this.ctx.bezierCurveTo(
+        cloud.x + cloud.width / 4, cloud.y,
+        cloud.x + cloud.width / 2, cloud.y - cloud.height / 2,
+        cloud.x + cloud.width, cloud.y + cloud.height / 2
+    );
+    this.ctx.bezierCurveTo(
+        cloud.x + cloud.width * 1.2, cloud.y + cloud.height / 2,
+        cloud.x + cloud.width * 1.2, cloud.y + cloud.height * 0.8,
+        cloud.x + cloud.width, cloud.y + cloud.height
+    );
+    this.ctx.bezierCurveTo(
+        cloud.x + cloud.width / 2, cloud.y + cloud.height * 1.3,
+        cloud.x + cloud.width / 4, cloud.y + cloud.height * 1.3,
+        cloud.x, cloud.y + cloud.height
+    );
+    this.ctx.bezierCurveTo(
+        cloud.x - cloud.width * 0.2, cloud.y + cloud.height * 0.8,
+        cloud.x - cloud.width * 0.2, cloud.y + cloud.height / 2,
+        cloud.x, cloud.y + cloud.height / 2
+    );
+
+    this.ctx.closePath();
+    this.ctx.fillStyle = '#E4E3EB';
+    this.ctx.fill();
+}
+
+drawClouds() {
+    this.clouds.forEach(cloud => {
+        cloud.x += cloud.speed; // Move the cloud horizontally
+        if (cloud.x > this.canvas.width) { // Reset cloud position when it goes beyond canvas width
+            cloud.x = -cloud.width;
+        }
+        this.drawCloud(cloud);
+    });
+}
+
+
 }
